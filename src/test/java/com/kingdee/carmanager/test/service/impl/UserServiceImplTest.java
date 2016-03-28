5a383305ca9fb85060a9f23a15fb3034fa67b026
jiangnenghua <568263091@qq.com>
2016-03-29 01:43:15 +0800

    merger

--- src/test/java/com/kingdee/carmanager/test/service/impl/UserServiceImplTest.java
+++ src/test/java/com/kingdee/carmanager/test/service/impl/UserServiceImplTest.java
@@ -1,16 +1,10 @@
 package com.kingdee.carmanager.test.service.impl;
 
-<<<<<<< HEAD
+
 import com.kingdee.carmanager.test.IDao.UserMapper;
 import com.kingdee.carmanager.test.bean.User;
 import com.kingdee.carmanager.test.service.UserService;
 import junit.framework.TestCase;
-import org.apache.ibatis.annotations.Select;
-import org.junit.After;
-=======
-import junit.framework.TestCase;
-
->>>>>>> origin/master
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +15,6 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-
-import com.kingdee.carmanager.test.service.UserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
