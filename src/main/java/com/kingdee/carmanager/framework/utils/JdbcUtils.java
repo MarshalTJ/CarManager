15aba8a5d90e1012d10d0865fe69341955f1b9b4
jiangnenghua <568263091@qq.com>
2016-03-31 15:02:26 +0800

    思来想去,还是用spring自己里面的datasource得了,不过还是读取配置文件比较保险,不用启动容器,快快哒

--- src/main/java/com/kingdee/carmanager/framework/utils/JdbcUtils.java
+++ src/main/java/com/kingdee/carmanager/framework/utils/JdbcUtils.java
@@ -1,15 +1,17 @@
 package com.kingdee.carmanager.framework.utils;
 
-import com.jolbox.bonecp.BoneCPDataSource;
-import org.apache.log4j.Logger;
-
-
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.log4j.Logger;
 
 /**
  * 默认使用classpath下的jdbc.properties
@@ -17,11 +19,13 @@ import java.util.Properties;
  */
 public class JdbcUtils {
     private static Logger logger = Logger.getLogger(JdbcUtils.class.getSimpleName());
-    private static BoneCPDataSource ds;
+    
+    private static BasicDataSource ds;
+    
     //使用ThreadLocal存储当前线程中的Connection对象 解决多线程之间的冲突
     private static ThreadLocal<Connection> localThread = new ThreadLocal<Connection>();
 
-    static {
+    static{
         Properties properties = new Properties();
         InputStream inputStream = Object.class.getResourceAsStream("/jdbc.properties");
         try {
@@ -35,18 +39,23 @@ public class JdbcUtils {
             String maxIdle = properties.getProperty("maxIdle");
             String minIdle = properties.getProperty("minIdle");
             String maxWait = properties.getProperty("maxWait");
-            ds = new BoneCPDataSource();
-            ds.setDriverClass(driverClass);
-            ds.setJdbcUrl(url);
+            ds = new BasicDataSource();
+            ds.setDriverClassName(driverClass);
+            ds.setUrl(url);
             ds.setUsername(username);
             ds.setPassword(password);
-            ds.setIdleMaxAgeInSeconds(Long.parseLong(maxIdle));
+            ds.setInitialSize(Integer.parseInt(initialSize));
+            ds.setMaxActive(Integer.parseInt(maxActive));
+            ds.setMaxIdle(Integer.parseInt(maxIdle));
+            ds.setMinIdle(Integer.parseInt(minIdle));
+            ds.setMaxWait(Long.parseLong(maxWait));
 
         } catch (IOException e) {
             logger.error("can't find jdbc.properties in classpath");
         }
     }
-
+    
+    
     public static DataSource getDataSource() {
         return ds;
     }
@@ -65,7 +74,25 @@ public class JdbcUtils {
      *
      * @throws SQLException
      */
-    public static void close() {
+    public static void cleanup(ResultSet rs, Statement stmt) {
+    	if( rs != null){
+    		try {
+				rs.close();
+			} catch (SQLException e) {
+				logger.error("rs close error", e);
+			}
+    		rs = null;
+    	}
+    	
+    	if( stmt != null){
+    		try {
+				stmt.close();
+			} catch (SQLException e) {
+				logger.error("stmt close error", e);
+			}
+    		stmt = null;
+    	}
+    	
         try {
             Connection conn = localThread.get();
             if (conn != null) {
@@ -78,6 +105,18 @@ public class JdbcUtils {
 
     }
 
+    public static void cleanup(ResultSet rs){
+    	cleanup(rs, null);
+    }
+    
+    public static void cleanup(Statement stmt){
+    	cleanup(null, stmt);
+    }
+    
+    public static void cleanup(){
+    	cleanup(null, null);
+    }
+    
     /**  以下是事物的一些操作**/
 
     public static void beginTrasaction() {
