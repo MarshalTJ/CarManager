9ea3c043719c09bb457860fbb3a1f7168a297f1e
renyi <yi_ren@kingdee.com>
2016-04-01 16:46:27 +0800

    提交测试

---
+++ src/main/java/com/carmanager/webapp/wechat/service/impl/WeixinServiceImpl.java
@@ -0,0 +1,9 @@
+package com.carmanager.webapp.wechat.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.carmanager.webapp.wechat.service.WeixinService;
+@Service
+public class WeixinServiceImpl implements WeixinService {
+
+}
