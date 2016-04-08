856081f584233cc11d879ef8c8c11c2f07870f21
renyi <yi_ren@kingdee.com>
2016-04-08 09:09:31 +0800

    20160408security  微信部分

--- src/main/java/com/carmanager/webapp/wechat/service/impl/WeixinServiceImpl.java
+++ src/main/java/com/carmanager/webapp/wechat/service/impl/WeixinServiceImpl.java
@@ -1,9 +1,21 @@
 package com.carmanager.webapp.wechat.service.impl;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.carmanager.webapp.wechat.bean.WeixinInfo;
+import com.carmanager.webapp.wechat.dao.WeixinDao;
 import com.carmanager.webapp.wechat.service.WeixinService;
 @Service
 public class WeixinServiceImpl implements WeixinService {
+	@Autowired
+	private WeixinDao weixinDao;
+
+	@Override
+	public WeixinInfo findWeixinInfoByAppid(Map<String, String> paraMap) {
+		return weixinDao.findWeixinInfoByAppid(paraMap);
+	}
 
 }
