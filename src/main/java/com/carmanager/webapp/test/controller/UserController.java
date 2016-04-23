f694957d623c1b885a3431902e90c11989be8b88
jiangnenghua <568263091@qq.com>
2016-04-23 13:57:27 +0800

    增加自己的权限拦截器。

--- src/main/java/com/carmanager/webapp/test/controller/UserController.java
+++ src/main/java/com/carmanager/webapp/test/controller/UserController.java
@@ -1,17 +1,20 @@
 package com.carmanager.webapp.test.controller;
 
-import com.carmanager.webapp.test.bean.User;
-import com.carmanager.webapp.test.service.UserService;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+import com.carmanager.webapp.permission.Permission;
+import com.carmanager.webapp.permission.PermissionInterceptor;
+import com.carmanager.webapp.test.bean.User;
 
 /**
  * Created by Administrator on 2016/3/29.
@@ -34,4 +37,38 @@ public class UserController {
     	logger.debug("Received request to show admin page");
     	return "adminpage";
     }
+    
+    @RequestMapping("/login")  
+    @ResponseBody  
+    public boolean login(HttpSession session,User user){  
+        boolean result=false;  
+        //模拟从数据库查出存在这样的用户  
+        Integer userId=user.getFid();  
+        if(userId!=null&&userId>0){  
+            session.setAttribute(PermissionInterceptor.SESSION_USERID, userId);  
+            session.setAttribute(PermissionInterceptor.SESSION_AUTHS, new HashSet<String>(Arrays.asList("user_list", "user_query", "user_save")));  
+            result=true;  
+        }  
+        return result;  
+    }  
+    @Permission("user_queryXXXX")  
+    @RequestMapping("/query")  
+    @ResponseBody  
+    public String query(){  
+        System.out.println("query");  
+        return getClass().toString();  
+    }  
+    @Permission("user_list")  
+    @RequestMapping("/list")  
+    @ResponseBody  
+    public String list(){  
+        System.out.println("list");  
+        return getClass().toString();  
+    }  
+    @Permission("user_save")  
+    @RequestMapping("/add")  
+    public String add(User user){  
+        System.out.println("add:"+user);  
+        return "user/add";  
+    }  
 }
